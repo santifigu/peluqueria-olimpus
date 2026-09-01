ba1e506e5c1e9d919ca9c4668874f390d8da6064
Santiago <santiago@example.com>
2026-09-01 16:18:51 -0300

    arreglar archivos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render  # type: ignore
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
--- prjolimpus/settings.py
+++ prjolimpus/settings.py
@@ -115,13 +115,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
